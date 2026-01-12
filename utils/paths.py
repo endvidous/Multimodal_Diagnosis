af55f281c2d3eb567d1806ec1624c520601a9b3c
Henry-Endvidous <henrypunnoose2004@gmail.com>
2026-01-13 01:51:52 +0530

    Paths fix for google drive

--- utils/paths.py
+++ utils/paths.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 def get_data_root():
     if Path("/content/drive").exists():
-        return Path("/content/drive/MyDrive/medical_ai_data")
+        return Path("/content/drive/MyDrive/Multi_Modal_Data/data")
     else:
         return Path(__file__).resolve().parents[1] / "data"
